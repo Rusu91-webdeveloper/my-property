aeae78241d97468ea9f573229c9033aa204202a5
Emanuel.Rusu <146270038+Rusu91-webdeveloper@users.noreply.github.com>
2024-08-12 10:55:28 +0200

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -255,11 +255,22 @@ const Navbar = () => {
             </Link>
           )}
 
-          {!session && (
-            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-              <i className="fa-brands fa-google mr-2"></i>
-              <span>Login or Register</span>
-            </button>
+       {!session && (
+            <div className="block md:ml-6">
+              <div className="flex items-center">
+                {providers &&
+                  Object.values(providers).map((provider) => (
+                    <button
+                      key={provider.name}
+                      onClick={() => signIn(provider.id)}
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-3"
+                    >
+                      <FaGoogle className="text-white mr-2" />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+              </div>
+            </div>
           )}
         </div>
       </div>
