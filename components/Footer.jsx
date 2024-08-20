cc172a589a7f9800fb30199c10ccde171f3ec895
Emanuel.Rusu <146270038+Rusu91-webdeveloper@users.noreply.github.com>
2024-08-20 14:17:14 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
         </div>
         <div>
           <p className="text-sm text-gray-500 mt-2 md:mt-0">
-            &copy; {currentYear} PropertyPulse. All rights reserved.
+            &copy; {currentYear} myProperty. All rights reserved.
           </p>
         </div>
       </div>
